aa70f73f79b67a9392352f64324ece0d16fba30c
Puredevs <azhar@puredevs.com>
2020-01-21 13:12:09 +0600

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -7,20 +6,32 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 var ImageminPlugin = require('imagemin-webpack-plugin').default;
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+  entry: {
+    app:'./src/index.js',
+    vendor: ['jquery','lodash']
   },
+  mode:'production',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'assets/js/[name][chunkhash].js'
+  },
+  optimization: {
+    splitChunks:{
+      chunks:'all'
+    },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
+
   module: {
     rules: [
-      {
+        {
         test: /\.(js|jsx)$/,
         use: {loader: 'babel-loader'},
-        exclude: /node_modules/
+        exclude: /node_modules/,
         },
         {
           test: /\.(sa|sc|c)ss$/,
@@ -31,14 +42,13 @@ module.exports = {
                 , 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap','postcss-loader']
                 
         },
-      
         {
           test: /\.(gif|png|jpe?g|svg|svg|woff|woff2|ttf|eot)$/i,
           use: [
               {
                 loader: 'url-loader',
                 options: {
-                  outputPath: 'images',
+                  outputPath: 'assets/images',
                   limit:154000,
                   name: '[name].[ext]?[hash]'
                 }
@@ -46,23 +56,24 @@ module.exports = {
               ,'image-webpack-loader']
       }
     ]
+  
   },
+
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
     new MiniCssExtractPlugin({
-      filename: "styles.css",
+      filename: "assets/css/styles.css",
     }),
     new HtmlWebpackPlugin({  // Also generate a test.html
-      template: path.resolve(__dirname, '/', 'index.html'),
+      template: path.resolve(__dirname, 'src/', 'index.html'),
       filename: 'index.html',
         inject: true
       }),
-    
       new ImageminPlugin(),
       new CleanWebpackPlugin()
-  ]
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
 };
