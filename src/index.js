20909f95b31b64ae6368cb29ab6e443445891faa
Puredevs <azhar@puredevs.com>
2020-01-21 15:28:21 +0600

    make it better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Index = () => {
+    return <div>Hello React! You are ready for heroku</div>;
+  };
+
+ReactDOM.render(<Index />, document.getElementById("root"));
 
