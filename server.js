2538313112df6053941d4a074caf8edf231b6488
Puredevs <azhar@puredevs.com>
2020-01-21 14:04:12 +0600

    make it better

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
+// console.log(path.resolve(__dirname, './public/index.html'));
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './src/index.html'));
+  res.sendFile(path.resolve(__dirname, './public/index.html'));
 });
 
 app.listen(port);
